abd78f2fcdcc9bcd83152caa1b0420dfd1148471
Main User Account <resler@ualberta.ca>
2017-02-28 19:40:05 -0700

    started heroku setup, got to ~step 7 of Lab 6 instructions

--- blockbuster/blockbuster/settings.py
+++ blockbuster/blockbuster/settings.py
@@ -74,12 +74,11 @@ WSGI_APPLICATION = 'blockbuster.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {'default': None}
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'] = db_from_env
 
 
 # Internationalization
@@ -99,4 +98,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, '../users/static'),
+	os.path.join(PROJECT_ROOT, '../posts/static'),	
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- blockbuster/blockbuster/wsgi.py
+++ blockbuster/blockbuster/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for blockbuster project.
+WSGI config for mysite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -13,4 +13,10 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blockbuster.settings")
 
+from whitenoise.django import DjangoWhiteNoise
+
+
+
 application = get_wsgi_application()
+
+application = DjangoWhiteNoise(application)
