a2f1b6afd39fe318a31fa571ccbb1089b4d4d37d
Aaron <supermanaaronliu@gmail.com>
2017-03-06 12:03:49 -0700

    update

--- blockbuster/blockbuster/settings.py
+++ blockbuster/blockbuster/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'blockbuster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'static/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,7 +112,7 @@ STATICFILES_DIRS = (
 # this is to integrate webpack/react with django
 WEBPACK_LOADER = {
     'DEFAULT': {
-        'BUNDLE_DIR_NAME': 'bundles',
+        'BUNDLE_DIR_NAME': 'bundles/',
         'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
     }
 }
