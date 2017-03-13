77a1d670166b8994181e827145a1feb89249678f
TheAspiredOne <altan@ualberta.ca>
2017-03-12 20:43:10 -0600

    registration dialog error

--- blockbuster/static/js/components/SubComponents/RegistrationDialog.js
+++ blockbuster/static/js/components/SubComponents/RegistrationDialog.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Dialog from 'material-ui/Dialog'
 import FlatButton from 'material-ui/FlatButton'
 import TextField from 'material-ui/TextField'
-import CreateUserReuqest from './CreateUserReuqest'
+import CreateUserRequest from './CreateUserRequest'
 
 
 export default class RegistrationDialog extends React.Component{
