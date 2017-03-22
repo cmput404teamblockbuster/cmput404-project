9ef146440472bb7ce2d4b04af2d4c1279e2a515d
Aaron <supermanaaronliu@gmail.com>
2017-03-22 00:55:29 -0600

    finished sigle post page

--- blockbuster/static/js/components/Pages/SinglePostPage/EmptyPost.js
+++ blockbuster/static/js/components/Pages/SinglePostPage/EmptyPost.js
@@ -6,10 +6,10 @@ export default class EmptyPost extends React.Component{
 
     render(){
         return(
-            <li>
+            <li style={{textAlign:"center"}}>
                 <Card className="textField">
                     <CardText >
-                        <p style={{fontSize:'3em', testAlign:"center"}} >
+                        <p style={{fontSize:'3em', }} >
                             404 POST NOT FOUND
                         </p>
                     </CardText>
