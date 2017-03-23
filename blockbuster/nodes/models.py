df86c48b4bbedfaaaabd7f6829ea14870afe2117
Sam Bao <sam.b5687@gmail.com>
2017-03-23 16:57:11 -0600

    rename node model

--- blockbuster/nodes/models.py
+++ blockbuster/nodes/models.py
@@ -7,5 +7,5 @@ from django.db import models
 
 class node(models.Model):
     host = models.URLField(max_length=500)
-    is_allowed = models.BooleanField()
+    is_allowed = models.BooleanField(default = True)
     
