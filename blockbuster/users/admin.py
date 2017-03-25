24b1dc5ac89ab35aa368e5a933171505314aa62d
Taylor Arnett <tarnett1@mac.com>
2017-03-25 15:07:38 -0600

    node auth

--- blockbuster/users/admin.py
+++ blockbuster/users/admin.py
@@ -7,17 +7,17 @@ from users.models import Profile, UserRelationship
 # Code from https://docs.djangoproject.com/en/1.10/topics/auth/customizing/#extending-the-existing-user-model on March 8, 2017
 
 
-class ProfileInline(admin.StackedInline):
-    model = Profile
-    can_delete = False
-    verbose_name_plural = 'profile'
-
-# Define a new User admin
-class UserAdmin(BaseUserAdmin):
-    inlines = (ProfileInline, )
+# class ProfileInline(admin.StackedInline):
+#     model = Profile
+#     can_delete = False
+#     verbose_name_plural = 'profile'
+#
+# # Define a new User admin
+# class UserAdmin(BaseUserAdmin):
+#     inlines = (ProfileInline, )
 
 # Re-register UserAdmin
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+# admin.site.register(User)
+admin.site.register(Profile)
 admin.site.register(UserRelationship)
 
