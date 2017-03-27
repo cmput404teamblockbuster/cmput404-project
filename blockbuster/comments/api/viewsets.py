bde295718893be65b50251731e7ee00cae71ac8f
Aaron <supermanaaronliu@gmail.com>
2017-03-26 21:19:02 -0600

    working

--- blockbuster/comments/api/viewsets.py
+++ blockbuster/comments/api/viewsets.py
@@ -27,7 +27,8 @@ class CommentViewSet(viewsets.ModelViewSet):
     def create(self, request, uuid):
         data = request.data
         serializer = CommentSerializer(data=data)
-        host = request.data.pop('host')
+        print data
+        host = data.get('host')
         if serializer.is_valid():
             try:
                 post = Post.objects.get(uuid=uuid)  # Get the post the comment is for
@@ -35,36 +36,39 @@ class CommentViewSet(viewsets.ModelViewSet):
                 node = Node.objects.filter(host=host)
                 if node and node[0].is_allowed:
                     node = node[0]
-                    api_url = host + 'api/posts/' + uuid + '/comments'
+                    api_url = host + 'api/posts/' + uuid + '/comments/'
                     response = requests.post(api_url, json = data, auth=(node.username_for_node, node.password_for_node))
                     if 199< response.status_code <300:
                         comment = response.json()
                         return Response(status = status.HTTP_200_OK, data = comment)
-                    print response.text
+                    file = open('out.txt','w')
+                    file.write(response.text)
+                    file.close()
+
                     return Response(status = status.HTTP_400_BAD_REQUEST, data = response)
                 else:
                     return Response(status = status.HTTP_401_UNAUTHORIZED, data = 'Comment from an unaccepted server')  
 
-            author = request.user.profile
-            if not post.viewable_for_author(author):
-                status_code = status.HTTP_403_FORBIDDEN
-                response_msg = dict(
-                    query='addComent',
-                    success=False,
-                    message='Comment not allowed'
-                )
-            else:
-                Comment.objects.create(
-                    author=author,
-                    body=serializer.data['comment'],
-                    post=post,
-                )
-                status_code = status.HTTP_201_CREATED
-                response_msg = dict(
-                    query='addComent',
-                    success=True,
-                    message='Comment Added'
-                )
+            # author = request.user.profile
+            # if not post.viewable_for_author(author):
+            #     status_code = status.HTTP_403_FORBIDDEN
+            #     response_msg = dict(
+            #         query='addComent',
+            #         success=False,
+            #         message='Comment not allowed'
+            #     )
+            # else:
+            Comment.objects.create(
+                author=data.get('author'),
+                body=serializer.data['comment'],
+                post=post,
+            )
+            status_code = status.HTTP_201_CREATED
+            response_msg = dict(
+                query='addComent',
+                success=True,
+                message='Comment Added'
+            )
 
             return Response(status=status_code, data=response_msg)
 
