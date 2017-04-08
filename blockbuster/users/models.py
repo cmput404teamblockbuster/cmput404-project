17d8d44ad6280369183ceb5beabf7e32a2475847
Aaron <supermanaaronliu@gmail.com>
2017-04-08 16:14:23 -0600

    chage the url part in the author model

--- blockbuster/users/models.py
+++ blockbuster/users/models.py
@@ -57,7 +57,10 @@ class Profile(models.Model):
         """
         returns a link to the users profile on our website
         """
-        return '%sprofile/%s/' % (str(self.host), str(self.uuid))
+        host = site_name
+        if 'api/' in site_name:
+            host = host[:-4]
+        return '%sprofile/%s/' % (host, str(self.uuid))
 
     @property
     def api_id(self):
