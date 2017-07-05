8e1e09b8a24981391d86a7cd869030fcc872d222
Taylor Arnett <tarnett1@mac.com>
2017-07-05 17:56:23 -0600

    test commit

--- blockbuster/comments/api/serializers.py
+++ blockbuster/comments/api/serializers.py
@@ -3,23 +3,3 @@ from comments.models import Comment
 from users.api.serializers import CondensedProfileSerializer
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    # http://www.django-rest-framework.org/api-guide/relations/#nested-relationships
-    author = CondensedProfileSerializer()
-    published = serializers.DateTimeField(source='created', required=False)
-    id = serializers.CharField(source='uuid', required=False)
-    comment = serializers.CharField(source='body')
-
-    class Meta:
-        model = Comment
-        fields = ('published', 'comment', 'author', 'id', 'contentType') # These fields will be available to the front end
-
-    def validate(self, data):
-        data = super(CommentSerializer, self).validate(data)
-        """
-        validate data here
-        """
-        if len(data.get('body')) == 0:
-            raise serializers.ValidationError('The comment has no body.')
-
-        return data
